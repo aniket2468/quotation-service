a6f8db35d80ae70531227beb60e028980f4b39c4
Aniket Sharma <aniketsharma2468@gmail.com>
2024-03-07 19:51:10 -0500

    index file update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ const FILE_STORAGE_PORT = parseInt(process.env.FILE_STORAGE_PORT);
 
 const app = express();
 
+app.get("/image", async (req, res) => {
+    
+    const imagePath = req.query.path;
+    const forwardRequest = http.request(
+        {
+            host: FILE_STORAGE_HOST,
+            port: FILE_STORAGE_PORT,
+            path: `/image?path=${imagePath}`,
+            method: 'GET',
+            headers: req.headers
+        },
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
+            }
+        );
+        req.pipe(forwardRequest);
+});
+
 app.get("/quotation", async (req, res) => {
     let quotation = await selectAQuotation();
     res.status(200).json(quotation);
